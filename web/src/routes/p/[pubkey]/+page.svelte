f97895894fe6f0bb0be7f2033e2bdb2312b68dd9
SnowCait <SnowCait@users.noreply.github.com>
2023-03-06 14:01:02 +0900

    Profile

--- web/src/routes/p/[pubkey]/+page.svelte
+++ web/src/routes/p/[pubkey]/+page.svelte
@@ -18,18 +18,50 @@
 </svelte:head>
 
 <main>
-	<section class="profile">
-		<img src={user.picture} alt="" />
-		<h1>{user.display_name} @{user.name}</h1>
-		<p>{user.about}</p>
+	<section>
+		<div class="banner">
+			<img src={user.banner} alt="" />
+		</div>
+		<div class="profile">
+			<img src={user.picture} alt="" />
+			<h1>{user.display_name ?? user.name ?? ''}</h1>
+			{#if user.name}
+				<h2>@{user.name}</h2>
+			{/if}
+			{#if user.about}
+				<pre>{user.about}</pre>
+			{/if}
+		</div>
 	</section>
 </main>
 
 <style>
+	.banner img {
+		object-fit: cover;
+		width: 100%;
+		height: 200px;
+	}
+
 	.profile img {
 		width: 128px;
 		height: 128px;
 		border-radius: 50%;
 		margin-right: 12px;
+	}
+
+	.profile h1 {
+		margin: 0;
+	}
+
+	.profile h2 {
+		margin: 0;
+		color: gray;
+		font-size: 1em;
+	}
+
+	.profile pre {
+		line-height: 1.5em;
+		white-space: pre-wrap;
+		word-break: break-all;
 	}
 </style>
