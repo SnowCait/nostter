60e4b913cd9480acf748d311378c657be5ae9a46
SnowCait <SnowCait@users.noreply.github.com>
2023-03-11 06:41:16 +0900

    Timeout

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -3,7 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		timeout: 300000
 	},
 	testDir: 'tests'
 };
