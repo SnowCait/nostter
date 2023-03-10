20262e42e523078ee94680b60d8d7f4ca3ed8c9d
SnowCait <SnowCait@users.noreply.github.com>
2023-03-11 06:15:47 +0900

    Menu

---
+++ web/src/routes/Header.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { IconHome, IconSearch, IconUser } from '@tabler/icons-svelte';
+	import { pubkey } from '../stores/Author';
+</script>
+
+<nav>
+	<ol>
+		<a href="/home">
+			<li>
+				<IconHome size={30} />
+			</li>
+		</a>
+		<a href="/search">
+			<li>
+				<IconSearch size={30} />
+			</li>
+		</a>
+		{#if $pubkey}
+			<a href="/p/{$pubkey}">
+				<li>
+					<IconUser size={30} />
+				</li>
+			</a>
+		{/if}
+	</ol>
+</nav>
+
+<style>
+	ol {
+		list-style: none;
+		padding: 0;
+	}
+
+	li {
+		width: 50px;
+		height: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	a:visited {
+		color: inherit;
+	}
+</style>
