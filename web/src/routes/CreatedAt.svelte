4a77168d394d2e04edb1c9e4a296bd71e9326ff9
SnowCait <SnowCait@users.noreply.github.com>
2023-03-11 12:30:48 +0900

    Created at

---
+++ web/src/routes/CreatedAt.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let createdAt: number;
+	const date = new Date(createdAt * 1000);
+	const elapsedTime = Date.now() - date.getTime();
+	const oneDay = 24 * 60 * 60* 1000;
+	let createdAtDisplay: string;
+	if (elapsedTime < oneDay) {
+		createdAtDisplay = date.toLocaleTimeString('ja-JP', {
+			hour: 'numeric',
+			minute: '2-digit'
+		});
+	} else if (elapsedTime < 30 * oneDay) {
+		createdAtDisplay = date.toLocaleDateString('ja-JP', {
+			month: 'numeric',
+			day: 'numeric'
+		});
+	} else {
+		createdAtDisplay = date.toLocaleDateString('ja-JP', {
+			year: 'numeric',
+			month: 'numeric',
+		});
+	}
+</script>
+
+<span>{createdAtDisplay}</span>
+
+<style>
+	span {
+		color: gray;
+		font-size: 0.5em;
+	}
+</style>
