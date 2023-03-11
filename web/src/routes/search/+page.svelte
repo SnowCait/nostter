6c7028988bf5ad397518ed325d472c19666ea871
SnowCait <SnowCait@users.noreply.github.com>
2023-03-11 12:31:20 +0900

    Stop propagation

--- web/src/routes/search/+page.svelte
+++ web/src/routes/search/+page.svelte
@@ -107,7 +107,7 @@
 <main>
 	<h1><a href="/search">Search</a></h1>
 	<form action="/search">
-		<input type="text" name="q" value={query} />
+		<input type="text" name="q" value={query} on:keyup|stopPropagation={() => {}} />
 		<input type="submit" value="Search" />
 	</form>
 
