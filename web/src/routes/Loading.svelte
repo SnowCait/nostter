e74029d652f176958e05884598f172e3430b45df
SnowCait <SnowCait@users.noreply.github.com>
2023-03-26 18:57:58 +0900

    Loading

---
+++ web/src/routes/Loading.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { IconLoader } from '@tabler/icons-svelte';
+</script>
+
+<span class="loading"><IconLoader /></span>
+
+<style>
+	.loading {
+		display: inline-block;
+		width: 24px;
+		height: 24px;
+		animation: 3s linear infinite rotation;
+	}
+
+	@keyframes rotation {
+		0% {
+			transform: rotate(0);
+		}
+
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
