cbcb5a408118c8b717866cf016f66c6324d9a40b
SnowCait <SnowCait@users.noreply.github.com>
2023-03-28 02:42:42 +0900

    Logout

--- web/src/routes/preference/+page.svelte
+++ web/src/routes/preference/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import Notification from './Notification.svelte';
 	import ReactionEmoji from './ReactionEmoji.svelte';
+	import Logout from '../Logout.svelte';
 </script>
 
 <h1>Preference</h1>
 
 <div><ReactionEmoji /></div>
 <div><Notification /></div>
+<div><Logout /></div>
 
 <style>
 	div {
