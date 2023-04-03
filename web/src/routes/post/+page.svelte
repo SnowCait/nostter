cf805243a42de19461adc1bd91f476109d74460f
SnowCait <SnowCait@users.noreply.github.com>
2023-04-04 01:43:02 +0900

    Intent

---
+++ web/src/routes/post/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { intentContent, openNoteDialog } from '../../stores/NoteDialog';
+	import { goto } from '$app/navigation';
+
+	onMount(async () => {
+		const content = $page.url.searchParams.get('content');
+		if (content !== null) {
+			$intentContent = content;
+			$openNoteDialog = true;
+		}
+
+		await goto('/');
+	});
+</script>
