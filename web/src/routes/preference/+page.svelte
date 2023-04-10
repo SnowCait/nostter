5b3394942bf7bce2a5be91183e02240dc4a4f7b7
SnowCait <SnowCait@users.noreply.github.com>
2023-04-11 02:55:40 +0900

    nostter-chan

--- web/src/routes/preference/+page.svelte
+++ web/src/routes/preference/+page.svelte
@@ -12,18 +12,29 @@
 
 <h1>About nostter</h1>
 <div>
-	<span>Author:</span>
-	<a href="/npub1s02jksmr6tgmcksf3hnmue7pyzlm0sxwarh7lk8tdeprw2hjg6ysp7fxtw">@SnowCait</a>
-</div>
-<div>
 	<span>GitHub:</span>
 	<a href="https://github.com/SnowCait/nostter" target="_blank" rel="noopener noreferrer">
 		SnowCait/nostter
 	</a>
 </div>
+<div>
+	<span>Author:</span>
+	<a href="/npub1s02jksmr6tgmcksf3hnmue7pyzlm0sxwarh7lk8tdeprw2hjg6ysp7fxtw">@SnowCait</a>
+</div>
+<div>
+	<span>Illustration:</span>
+	<a href="/npub1e09suzmq9mp6nt0ud9ttl03790qjx70wzwlc2pwwghcusvwju54qs0c800">@stok33</a>
+</div>
+<img src="/nostter-chan.jpg" alt="" title="前から知っていたような雰囲気ですぐ馴染めるタイプの娘。パーカーの中には json が格納されていて聞くと教えてくれるよ。あとかなり物覚えが良いので昔のことも覚えててくれるよ。">
 
 <style>
 	div {
 		margin: 1em auto;
+	}
+
+	img {
+		width: 50%;
+		border: 1px solid lightgray;
+		border-radius: 10px;
 	}
 </style>
