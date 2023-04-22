5b49649dfee922fa266e847a8d225eaa011bcb3c
SnowCait <SnowCait@users.noreply.github.com>
2023-04-22 20:42:33 +0900

    Mute

--- web/src/stores/Author.ts
+++ web/src/stores/Author.ts
@@ -1,4 +1,4 @@
-import { writable, type Writable } from 'svelte/store';
+import { get, writable, type Writable } from 'svelte/store';
 import type { User } from '../routes/types';
 
 export const pubkey = writable('');
@@ -6,5 +6,8 @@ export const authorProfile: Writable<User> = writable();
 export const recommendedRelay = writable('');
 export const followees: Writable<string[]> = writable([]);
 export const mutePubkeys: Writable<string[]> = writable([]);
+export const pinNotes: Writable<string[]> = writable([]);
 export const relayUrls: Writable<string[]> = writable([]);
 export const rom = writable(false);
+
+export const isMutePubkey = (pubkey: string) => get(mutePubkeys).includes(pubkey);
