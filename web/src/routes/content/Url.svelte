360b4a66953f3db3575d1ba36defc25cf10d6d6f
SnowCait <SnowCait@users.noreply.github.com>
2023-05-07 05:38:35 +0900

    Show media as a part of content

--- web/src/routes/content/Url.svelte
+++ web/src/routes/content/Url.svelte
@@ -1,8 +1,45 @@
 <script lang="ts">
 	import { IconExternalLink } from '@tabler/icons-svelte';
+	import Text from './Text.svelte';
 
 	export let text: string;
 	export let url: string = text;
+
+	let link: URL | undefined;
+	try {
+		link = new URL(url);
+	} catch (error) {
+		console.log('[invalid url]', url, error);
+	}
 </script>
 
-<a href={url} target="_blank" rel="noopener noreferrer">{text}<IconExternalLink size={15} /></a>
+{#if link === undefined}
+	<Text {text} />
+{:else if /\.(apng|avif|gif|jpg|jpeg|png|webp|bmp)$/i.test(link.pathname)}
+	<a href={link.href} target="_blank" rel="noreferrer">
+		<img src={link.href} alt="" />
+	</a>
+{:else if /\.(mp3|m4a|wav)/i.test(link.pathname)}
+	<audio src={link.href} controls />
+{:else if /\.(mp4|ogg|webm|ogv|mov|mkv|avi|m4v)/i.test(link.pathname)}
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<video src={link.href} controls />
+{:else}
+	<a href={link.href} target="_blank" rel="noopener noreferrer">
+		{text}<IconExternalLink size={15} />
+	</a>
+{/if}
+
+<style>
+	img,
+	video {
+		max-width: calc(100% - 1.5em);
+		max-height: 20em;
+		margin: 0.5em;
+	}
+
+	img {
+		border: 1px solid lightgray;
+		border-radius: 5px;
+	}
+</style>
