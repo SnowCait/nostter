c3f1ce35293f2aeb3fac801b0c421872eb8e748a
SnowCait <SnowCait@users.noreply.github.com>
2023-05-06 09:59:41 +0900

    Fix parameter RegExp

--- web/src/params/npub.ts
+++ web/src/params/npub.ts
@@ -1,5 +1,5 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
 export const match = ((param) => {
-	return /^(npub|nprofile)1[a-z0-9]+$/.test(param) || /^[\w-.]+@[\w-.]+$/.test(param);
+	return /^(npub|nprofile)1[a-z0-9]{6,}$/i.test(param) || /^[\w-.]+@[\w-.]+$/.test(param);
 }) satisfies ParamMatcher;
