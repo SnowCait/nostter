2ea28f9e65814d70c8784c32304cbee93c671fa5
ys3669 <ys3669jp@gmail.com>
2023-05-06 10:52:33 +0900

    Change preprocesser vitePreprocess -> svelte-preprocess

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,26 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
+//import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const tagsRegex1 = /(>)[\s]*([<{])/g;
+const tagsRegex2 = /({[/:][a-z]+})[\s]*([<{])/g;
+const tagsRegex3 = /({[#:][a-z]+ .+?})[\s]*([<{])/g;
+const tagsRegex4 = /([>}])[\s]+(<|{[/#:][a-z][^}]*})/g;
+const tagsReplace = '$1$2';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	//  preprocess: [vitePreprocess()]
+	preprocess: preprocess({
+		replace: [
+			[tagsRegex1, tagsReplace],
+			[tagsRegex2, tagsReplace],
+			[tagsRegex3, tagsReplace],
+			[tagsRegex4, tagsReplace]
+		]
+	}),
 	kit: {
 		adapter: adapter()
 	}
