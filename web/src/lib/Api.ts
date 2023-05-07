3cf81d3c0e315abf4b65845d43058b82f6286c52
SnowCait <SnowCait@users.noreply.github.com>
2023-05-08 08:16:40 +0900

    Clear timeout

--- web/src/lib/Api.ts
+++ web/src/lib/Api.ts
@@ -187,7 +187,7 @@ export class Api {
 
 			const done = () => Array.from(publishedRelays).some(([, ok]) => ok);
 
-			setTimeout(() => {
+			const timeoutId = setTimeout(() => {
 				console.warn(
 					'[publish timeout]',
 					this.relays.filter((relay) => !publishedRelays.has(relay)),
@@ -201,6 +201,7 @@ export class Api {
 				console.log('[ok]', relay, `${Date.now() - now}ms`);
 				publishedRelays.set(relay, true);
 				if (this.relays.length === publishedRelays.size) {
+					clearTimeout(timeoutId);
 					resolve(done());
 				}
 			});
@@ -208,6 +209,7 @@ export class Api {
 				console.warn('[failed]', relay, `${Date.now() - now}ms`);
 				publishedRelays.set(relay, false);
 				if (this.relays.length === publishedRelays.size) {
+					clearTimeout(timeoutId);
 					resolve(done());
 				}
 			});
