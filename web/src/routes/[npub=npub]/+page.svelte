71bf467f56fde10d10925c2920ac9bf3d3a0d9de
SnowCait <SnowCait@users.noreply.github.com>
2023-07-12 04:59:38 +0900

    Extract badge logic

--- web/src/routes/[npub=npub]/+page.svelte
+++ web/src/routes/[npub=npub]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { error } from '@sveltejs/kit';
 	import { page } from '$app/stores';
-	import { nip19, SimplePool, type Event as NostrEvent } from 'nostr-tools';
+	import { nip19, SimplePool } from 'nostr-tools';
 	import type { Event, User } from '../types';
 	import { pool } from '../../stores/Pool';
 	import TimelineView from '../TimelineView.svelte';
@@ -9,6 +9,7 @@
 	import { afterNavigate } from '$app/navigation';
 	import Follow from '../Follow.svelte';
 	import { Api } from '$lib/Api';
+	import { BadgeApi, type Badge } from '$lib/BadgeApi';
 	import Loading from '../Loading.svelte';
 	import { User as UserDecoder } from '$lib/User';
 	import { Timeline } from '$lib/Timeline';
@@ -66,107 +67,12 @@
 			followers = pubkeys;
 			followersLoading = false;
 		});
-		fetchBadges(relays, pubkey).then((data) => {
+		const badgeApi = new BadgeApi();
+		badgeApi.fetchBadges(relays, pubkey).then((data) => {
 			badges = data;
 		});
 		await load();
 	});
-
-	async function fetchBadges(relays: string[], pubkey: string): Promise<Badge[]> {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				resolve([]);
-			}, 10000);
-
-			let profileBadges: ProfileBadge[] = [];
-			const subscribeProfileBadges = $pool.sub(relays, [
-				{
-					kinds: [30008],
-					authors: [pubkey]
-				}
-			]);
-			subscribeProfileBadges.on('event', (event: NostrEvent) => {
-				console.log('[profile badges event]', event);
-				if (
-					event.tags.some(([tagName, id]) => tagName === 'd' && id === 'profile_badges')
-				) {
-					for (let i = 0; i < event.tags.length; i++) {
-						const tag = event.tags[i];
-						if (tag[0] === 'd') {
-							continue;
-						}
-
-						if (tag[0] === 'a') {
-							const [kind, pubkey, badgeId] = tag[1].split(':');
-							i++;
-							const eTag = event.tags[i];
-							const [tagName, eventId, relay] = eTag;
-							if (tagName === 'e') {
-								const badge = {
-									kind,
-									pubkey,
-									badgeId,
-									eventId,
-									relay
-								} as ProfileBadge;
-								profileBadges.push(badge);
-							}
-						}
-					}
-				}
-			});
-			subscribeProfileBadges.on('eose', () => {
-				console.log('[profile badges]', profileBadges);
-				const pubkeys = new Set(profileBadges.map((x) => x.pubkey));
-				const subscribeBadgeDefinitions = $pool.sub(relays, [
-					{
-						kinds: [30009],
-						authors: Array.from(pubkeys)
-					}
-				]);
-				subscribeBadgeDefinitions.on('event', (event: NostrEvent) => {
-					console.log('[badge definitions event]', event);
-					const badge = {} as Badge;
-					for (const [tagName, tagContent] of event.tags) {
-						switch (tagName) {
-							case 'd':
-								badge.id = tagContent;
-								break;
-							case 'name':
-								badge.name = tagContent;
-								break;
-							case 'description':
-								badge.description = tagContent;
-								break;
-							case 'image':
-								badge.image = tagContent;
-								break;
-							case 'thumb':
-								badge.thumb = tagContent;
-								break;
-						}
-					}
-					console.log('[badge]', badge);
-					if (
-						profileBadges.some(
-							(x) => x.pubkey === event.pubkey && x.badgeId === badge.id
-						)
-					) {
-						badge.naddr = nip19.naddrEncode({
-							identifier: badge.id,
-							pubkey: event.pubkey,
-							kind: 30009
-						});
-						badges.push(badge);
-					}
-				});
-				subscribeBadgeDefinitions.on('eose', () => {
-					console.log('[badges]', badges);
-					resolve(badges);
-				});
-			});
-		});
-	}
 
 	async function load() {
 		if (timeline === undefined) {
@@ -195,23 +101,6 @@
 			count++;
 			console.log('[load]', count, until, seconds / 3600, events.length);
 		}
-	}
-
-	interface Badge {
-		id: string;
-		name: string;
-		description: string;
-		image: string;
-		thumb: string;
-		naddr: string;
-	}
-
-	interface ProfileBadge {
-		kind: string;
-		pubkey: string;
-		badgeId: string;
-		eventId: string;
-		relay: string;
 	}
 </script>
 
