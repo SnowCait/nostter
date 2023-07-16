b4ca16069188720ee1c2dc079acf0e0975dca3e0
SnowCait <SnowCait@users.noreply.github.com>
2023-07-17 04:49:39 +0900

    Ensure to save nsec

--- web/src/routes/Logout.svelte
+++ web/src/routes/Logout.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
+	import { loginType } from '../stores/Author';
+
+	let saved = false;
+
 	async function logout() {
 		localStorage.clear();
 		location.href = '/';
 	}
 </script>
 
-<button on:click={logout}>Logout</button>
+{#if $loginType === 'nsec'}
+	<h4>Ensure to save nsec before logout</h4>
+	<div>{localStorage.getItem('nostter:login')}</div>
+{/if}
+
+<button on:click={logout} disabled={$loginType === 'nsec' && !saved}>Logout</button>
+{#if $loginType === 'nsec'}
+	<label>
+		<input type="checkbox" bind:checked={saved} />
+		<span>Saved</span>
+	</label>
+{/if}
+
+<style>
+	h4 {
+		margin: 0.5rem auto;
+	}
+	button {
+		margin: 1rem auto;
+	}
+</style>
