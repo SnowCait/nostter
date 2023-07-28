d135c7034fcf0f7124c5278bb786828e667f3fa2
SnowCait <SnowCait@users.noreply.github.com>
2023-07-29 07:33:51 +0900

    Localize created_at

--- web/src/routes/CreatedAt.svelte
+++ web/src/routes/CreatedAt.svelte
@@ -2,22 +2,23 @@
 	export let createdAt: number;
 	export let format: 'auto' | 'time' = 'auto';
 
+	const lang = navigator.language;
 	const date = new Date(createdAt * 1000);
 	const elapsedTime = Date.now() - date.getTime();
 	const oneDay = 24 * 60 * 60 * 1000;
 	let createdAtDisplay: string;
 	if (elapsedTime < oneDay || format === 'time') {
-		createdAtDisplay = date.toLocaleTimeString('ja-JP', {
+		createdAtDisplay = date.toLocaleTimeString(lang, {
 			hour: 'numeric',
 			minute: '2-digit'
 		});
 	} else if (elapsedTime < 365 * oneDay) {
-		createdAtDisplay = date.toLocaleDateString('ja-JP', {
+		createdAtDisplay = date.toLocaleDateString(lang, {
 			month: 'numeric',
 			day: 'numeric'
 		});
 	} else {
-		createdAtDisplay = date.toLocaleDateString('ja-JP', {
+		createdAtDisplay = date.toLocaleDateString(lang, {
 			year: 'numeric',
 			month: 'numeric'
 		});
