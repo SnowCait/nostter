1103cca396537d86a1e3e479992ef03069d37d91
SnowCait <SnowCait@users.noreply.github.com>
2023-09-20 11:36:29 +0900

    i18n

---
+++ web/src/lib/i18n/index.ts
@@ -0,0 +1,12 @@
+import { browser } from '$app/environment';
+import { init, register } from 'svelte-i18n';
+
+const defaultLocale = 'en';
+
+register('en', () => import('./locales/en.json'));
+register('ja', () => import('./locales/ja.json'));
+
+init({
+	fallbackLocale: defaultLocale,
+	initialLocale: browser ? window.navigator.language : defaultLocale
+});
