1c7f9bda5ec721ff3df60175a41084de232b079f
SnowCait <SnowCait@users.noreply.github.com>
2023-09-23 12:02:11 +0900

    Fix import

--- web/src/routes/content/LongFormContent.svelte
+++ web/src/routes/content/LongFormContent.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Event } from 'nostr-tools';
+	import { nip19, type Event } from 'nostr-tools';
 	import { intentContent, openNoteDialog } from '../../stores/NoteDialog';
 	import IconCodeDots from '@tabler/icons-svelte/dist/svelte/icons/IconCodeDots.svelte';
 	import IconQuote from '@tabler/icons-svelte/dist/svelte/icons/IconQuote.svelte';
