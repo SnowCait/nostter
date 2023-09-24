1b48ed3cf7ab3edf448b86c38feb7ab3cbe86b49
SnowCait <SnowCait@users.noreply.github.com>
2023-09-24 22:44:08 +0900

    Format

--- web/src/routes/content/Reference.svelte
+++ web/src/routes/content/Reference.svelte
@@ -28,7 +28,7 @@
 			const api = new Api($pool, $readRelays);
 			const e = await api.fetchEventById(eventId);
 			if (e !== undefined) {
-				item = new EventItem(e, metadataEvents.get(e.pubkey))
+				item = new EventItem(e, metadataEvents.get(e.pubkey));
 			}
 		}
 	});
