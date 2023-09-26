959ea6a0efdae2e389677e798682bf3effe8112f
SnowCait <SnowCait@users.noreply.github.com>
2023-09-26 20:23:21 +0900

    Merge home and global timelines

--- web/src/routes/home/+page.svelte
+++ web/src/routes/home/+page.svelte
@@ -1,5 +1,23 @@
-<script>
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
 	import FollowingTimeline from './FollowingTimeline.svelte';
+	import UserFollowingTimeline from '../[npub=npub]/timeline/UserFollowingTimeline.svelte';
+	import { japaneseBotNpub } from '$lib/Constants';
+	import { nip19 } from 'nostr-tools';
+
+	let pubkey: string;
+	$: pubkey = nip19.decode(japaneseBotNpub).data as string;
+
+	let selected: 'home' | 'global' = 'home';
 </script>
 
-<FollowingTimeline />
+<select bind:value={selected}>
+	<option value="home">{$_('layout.header.home')}</option>
+	<option value="global">{$_('layout.header.global')}</option>
+</select>
+
+{#if selected === 'home'}
+	<FollowingTimeline />
+{:else if selected === 'global'}
+	<UserFollowingTimeline {pubkey} />
+{/if}
