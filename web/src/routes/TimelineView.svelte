8eb50934317f5ba49a9d744ec30eda192b315065
雪猫 <SnowCait@users.noreply.github.com>
2023-09-28 20:32:40 +0900

    Fix syntax error

--- web/src/routes/TimelineView.svelte
+++ web/src/routes/TimelineView.svelte
@@ -66,7 +66,7 @@
 					if (!refEventId) {
 						continue;
 					}
-					const noteId = nip19.neventEncode({ id: refEventId };
+					const noteId = nip19.neventEncode({ id: refEventId });
 					await goto(`/${noteId}`);
 					return;
 				}
