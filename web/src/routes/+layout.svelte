e33debd3e023d653ec8a8aadaa02f0832c71a743
studiokaiji <zisakutyuusennki@gmail.com>
2023-10-04 02:57:46 +0900

    outlinedとdisabled状態のbuttonのstyleを追加

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -170,12 +170,23 @@
 		opacity: 0.75;
 	}
 
-	:global(
-			button.button-small,
-			input[type='button'].button-small,
-			input[type='submit'].button-small
-		) {
-		padding: 0.5rem 1rem;
+	:global(.button-small) {
+		padding: 0.5rem 1.25rem;
+	}
+
+	:global(.button-outlined) {
+		background: transparent;
+		border: 1px solid var(--accent-gray);
+		color: var(--accent-gray);
+	}
+
+	:global(.button-outlined:hover) {
+		opacity: 1;
+		background: var(--accent-surface-high);
+	}
+
+	:global(button:disabled) {
+		opacity: 0.5;
 	}
 
 	:global(
