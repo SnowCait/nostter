160226b80e0561b15ed9e585893f4293d2cd4c21
SnowCait <SnowCait@users.noreply.github.com>
2023-10-12 20:43:34 +0900

    Upload media from file

---
+++ web/src/routes/editor/MediaPicker.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import IconPhoto from '@tabler/icons-svelte/dist/svelte/icons/IconPhoto.svelte';
+
+	export let mediaFiles: File[];
+
+	let input: HTMLInputElement | undefined;
+	let files: FileList;
+
+	function upload() {
+		console.log('[media upload]', files);
+		for (const file of files) {
+			console.log('[media upload file]', file);
+			mediaFiles.push(file);
+			mediaFiles = mediaFiles;
+		}
+	}
+</script>
+
+<button on:click={() => input?.click()} class="clear">
+	<IconPhoto size="30" />
+</button>
+<input type="file" bind:this={input} bind:files on:change={upload} accept="image/*" hidden>
+
+<style>
+	button {
+		color: var(--accent);
+	}
+</style>
