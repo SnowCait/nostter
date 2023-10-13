c1096492eb4d20c3bbad63647141d535e6981763
SnowCait <SnowCait@users.noreply.github.com>
2023-10-13 12:20:59 +0900

    Refactor media picker

--- web/src/routes/editor/MediaPicker.svelte
+++ web/src/routes/editor/MediaPicker.svelte
@@ -2,28 +2,16 @@
 	import { createEventDispatcher } from 'svelte';
 	import IconPhoto from '@tabler/icons-svelte/dist/svelte/icons/IconPhoto.svelte';
 
-	export let mediaFiles: File[] = [];
-
 	let input: HTMLInputElement | undefined;
 	let files: FileList;
 
 	const dispatch = createEventDispatcher();
-
-	function upload() {
-		dispatch('pick', files);
-		console.log('[media upload]', files);
-		for (const file of files) {
-			console.log('[media upload file]', file);
-			mediaFiles.push(file);
-			mediaFiles = mediaFiles;
-		}
-	}
 </script>
 
 <button on:click|preventDefault={() => input?.click()} class="clear">
 	<IconPhoto size="30" />
 </button>
-<input type="file" bind:this={input} bind:files on:change={upload} accept="image/*" hidden>
+<input type="file" bind:this={input} bind:files on:change={() => dispatch('pick', files)} accept="image/*" hidden>
 
 <style>
 	button {
