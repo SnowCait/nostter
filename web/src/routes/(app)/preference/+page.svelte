61e69ede72f5afce5c473593390f055c83479ce6
SnowCait <SnowCait@users.noreply.github.com>
2023-10-22 05:11:52 +0900

    UI: @kaiji

--- web/src/routes/(app)/preference/+page.svelte
+++ web/src/routes/(app)/preference/+page.svelte
@@ -62,6 +62,11 @@
 	<ZapButton pubkey="83d52b4363d2d1bc5a098de7be67c120bfb7c0cee8efefd8eb6e42372af24689" />
 </div>
 <div class="user">
+	<span>UI:</span>
+	<a href="/npub194qhhn5vzzyrsqaugfms8c7ycqjyvhyguurra450nhlweatfzxkqy8tgkd">@kaiji</a>
+	<ZapButton pubkey="2d417bce8c10883803bc427703e3c4c024465c88e7063ed68f9dfeecf56911ac" />
+</div>
+<div class="user">
 	<span>Illustration:</span>
 	<a href="/npub1e09suzmq9mp6nt0ud9ttl03790qjx70wzwlc2pwwghcusvwju54qs0c800">@stok33</a>
 	<ZapButton pubkey="cbcb0e0b602ec3a9adfc6956bfbe3e2bc12379ee13bf8505ce45f1c831d2e52a" />
