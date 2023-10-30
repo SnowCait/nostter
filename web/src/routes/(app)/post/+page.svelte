cace5bf2e2f599a96cd515f4042ccd4da2a50bf9
SnowCait <SnowCait@users.noreply.github.com>
2023-10-30 15:28:29 +0900

    Intent

--- web/src/routes/(app)/post/+page.svelte
+++ web/src/routes/(app)/post/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	import { intentContent, openNoteDialog } from '../../../stores/NoteDialog';
-	import { goto } from '$app/navigation';
+	import { intentContent } from '../../../stores/NoteDialog';
+	import NoteEditor from '../editor/NoteEditor.svelte';
 
 	onMount(async () => {
 		const content = $page.url.searchParams.get('content');
 		if (content !== null) {
 			$intentContent = content;
-			$openNoteDialog = true;
 		}
-
-		await goto('/');
 	});
 </script>
+
+<article class="card">
+	<NoteEditor />
+</article>
