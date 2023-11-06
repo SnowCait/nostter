246784ce8b92668a7a6e793b41baccac968ba6c2
SnowCait <SnowCait@users.noreply.github.com>
2023-11-06 15:13:34 +0900

    Fix naddr navigation

--- web/src/params/npub.ts
+++ web/src/params/npub.ts
@@ -1,9 +1,10 @@
 import type { ParamMatcher } from '@sveltejs/kit';
+import { match as matchNaddr } from './naddr';
 import { match as matchNote } from './note';
 
 export const match = ((param) => {
 	return (
 		/^(npub|nprofile)1[a-z0-9]{6,}$/i.test(param) ||
-		(!matchNote(param) && /^(?:[\w-.]+@)?[\w-.]+$/.test(param))
+		(!matchNote(param) && !matchNaddr(param) && /^(?:[\w-.]+@)?[\w-.]+$/.test(param))
 	);
 }) satisfies ParamMatcher;
