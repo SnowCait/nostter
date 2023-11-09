911762b2edf4a7ea6f7fb4516a3197f5683d383c
SnowCait <SnowCait@users.noreply.github.com>
2023-11-10 03:14:12 +0900

    Load page

---
+++ web/src/routes/(app)/[slug=naddr]/+page.ts
@@ -0,0 +1,27 @@
+import { nip19 } from 'nostr-tools';
+import type { AddressPointer } from 'nostr-tools/lib/nip19';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad<{
+	kind: number;
+	pubkey: string;
+	identifier: string;
+	relays: string[];
+}> = async ({ params }) => {
+	console.log('[naddr page load]', params.slug);
+	try {
+		const { data } = nip19.decode(params.slug);
+		console.log('[naddr decode]', data);
+		const pointer = data as AddressPointer;
+		return {
+			kind: pointer.kind,
+			pubkey: pointer.pubkey,
+			identifier: pointer.identifier,
+			relays: pointer.relays ?? []
+		};
+	} catch (e) {
+		console.error('[naddr page decode error]', e);
+		throw error(404, 'Not Found');
+	}
+};
