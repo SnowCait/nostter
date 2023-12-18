e228f2e3fe9115338bc9435991c05609018a474c
SnowCait <SnowCait@users.noreply.github.com>
2023-12-19 06:40:18 +0900

    About nostter

--- web/src/routes/(app)/preferences/+page.svelte
+++ web/src/routes/(app)/preferences/+page.svelte
@@ -12,22 +12,10 @@
 	import MutedEvents from './MutedEvents.svelte';
 	import ClearEmojiMartCache from './ClearEmojiMartCache.svelte';
 	import Theme from './Theme.svelte';
-	import ZapButton from '$lib/components/ZapButton.svelte';
 	import UriScheme from './UriScheme.svelte';
 	import EnablePreview from './EnablePreview.svelte';
 	import DeveloperMode from './DeveloperMode.svelte';
 	import WebStorage from './WebStorage.svelte';
-
-	let debugCounter = 0;
-
-	function enableDebugMode() {
-		debugCounter++;
-		if (debugCounter >= 5) {
-			$debugMode = !$debugMode;
-			debugCounter = 0;
-			console.log('[debug mode]', $debugMode ? 'enabled' : 'disabled');
-		}
-	}
 </script>
 
 <svelte:head>
@@ -63,54 +51,8 @@
 {/if}
 <div><Logout /></div>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<h1 on:click={enableDebugMode}>About nostter</h1>
-<div>
-	<span>GitHub:</span>
-	<a href="https://github.com/SnowCait/nostter" target="_blank" rel="noopener noreferrer">
-		SnowCait/nostter
-	</a>
-</div>
-<div class="user">
-	<span>Author:</span>
-	<a href="/npub1s02jksmr6tgmcksf3hnmue7pyzlm0sxwarh7lk8tdeprw2hjg6ysp7fxtw">@SnowCait</a>
-	<ZapButton pubkey="83d52b4363d2d1bc5a098de7be67c120bfb7c0cee8efefd8eb6e42372af24689" />
-</div>
-<div class="user">
-	<span>UI:</span>
-	<a href="/npub194qhhn5vzzyrsqaugfms8c7ycqjyvhyguurra450nhlweatfzxkqy8tgkd">@kaiji</a>
-	<ZapButton pubkey="2d417bce8c10883803bc427703e3c4c024465c88e7063ed68f9dfeecf56911ac" />
-</div>
-<div class="user">
-	<span>Logo:</span>
-	<a href="/npub1njty7prjt49kju6c3mjjlxvm0hjymf5s2qlfcyjqg5e0k8kftp3s4dpsn5">@apco</a>
-	<ZapButton pubkey="9c964f04725d4b6973588ee52f999b7de44da690503e9c12404532fb1ec95863" />
-</div>
-<div class="user">
-	<span>Illustration:</span>
-	<a href="/npub1e09suzmq9mp6nt0ud9ttl03790qjx70wzwlc2pwwghcusvwju54qs0c800">@stok33</a>
-	<ZapButton pubkey="cbcb0e0b602ec3a9adfc6956bfbe3e2bc12379ee13bf8505ce45f1c831d2e52a" />
-</div>
-<img src="/nostter-chan.jpg" alt="" />
-
 <style>
 	div {
 		margin: 1em auto;
-	}
-
-	img {
-		width: 50%;
-		border: 1px solid lightgray;
-		border-radius: 10px;
-	}
-
-	.user {
-		display: flex;
-		align-items: center;
-	}
-
-	.user * {
-		margin-right: 0.2rem;
 	}
 </style>
