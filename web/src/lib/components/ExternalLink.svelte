ce74ade3f1852209a37f7322098c45d50523ef25
SnowCait <SnowCait@users.noreply.github.com>
2023-12-27 02:42:00 +0900

    External link

---
+++ web/src/lib/components/ExternalLink.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import IconExternalLink from '@tabler/icons-svelte/dist/svelte/icons/IconExternalLink.svelte';
+
+	export let text: string = '';
+	export let link: URL;
+</script>
+
+<a href={link.href} target="_blank" rel="noopener noreferrer">
+	<slot>{text}</slot><IconExternalLink size={15} />
+</a>
