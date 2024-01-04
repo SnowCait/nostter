d9860512e2f4106e752ee1e10c0c64bd382b0219
SnowCait <SnowCait@users.noreply.github.com>
2024-01-05 04:48:21 +0900

    Refactor followees

--- web/src/lib/Contacts.ts
+++ web/src/lib/Contacts.ts
@@ -1,6 +1,9 @@
+import { get } from 'svelte/store';
 import { Kind, type SimplePool } from 'nostr-tools';
 import { Api } from './Api';
 import { WebStorage } from './WebStorage';
+import { filterTags } from './EventHelper';
+import { followees, pubkey } from '../stores/Author';
 
 export class Contacts {
 	private readonly api: Api;
@@ -40,6 +43,8 @@ export class Contacts {
 		tags.push(['p', pubkey]);
 
 		await this.api.signAndPublish(Kind.Contacts, contacts?.content ?? '', tags);
+
+		updateFollowees(tags);
 	}
 
 	public async unfollow(pubkey: string): Promise<void> {
@@ -50,11 +55,11 @@ export class Contacts {
 			return;
 		}
 
-		await this.api.signAndPublish(
-			Kind.Contacts,
-			contacts.content,
-			contacts.tags.filter(([tagName, p]) => !(tagName === 'p' && p === pubkey))
-		);
+		const tags = contacts.tags.filter(([tagName, p]) => !(tagName === 'p' && p === pubkey));
+
+		await this.api.signAndPublish(Kind.Contacts, contacts.content, tags);
+
+		updateFollowees(tags);
 	}
 
 	// For legacy clients
@@ -77,4 +82,11 @@ export class Contacts {
 
 		await this.api.signAndPublish(Kind.Contacts, content, contacts.tags);
 	}
+}
+
+export function updateFollowees(tags: string[][]): void {
+	const pubkeys = new Set(filterTags('p', tags));
+	pubkeys.add(get(pubkey)); // Add myself
+	followees.set([...pubkeys]);
+	console.log('[contacts]', pubkeys.size);
 }
