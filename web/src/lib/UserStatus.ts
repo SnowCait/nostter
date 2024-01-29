9339c3a0ab77b53dced52d0d1df35295b506c8ee
SnowCait <SnowCait@users.noreply.github.com>
2024-01-30 08:00:36 +0900

    Refactoring

--- web/src/lib/UserStatus.ts
+++ web/src/lib/UserStatus.ts
@@ -1,6 +1,6 @@
 import { get, writable } from 'svelte/store';
 import { batch, createRxBackwardReq, isExpired, uniq } from 'rx-nostr';
-import { bufferTime } from 'rxjs';
+import { bufferTime, filter } from 'rxjs';
 import type { Event } from 'nostr-typedef';
 import { rxNostr } from './timelines/MainTimeline';
 import { maxFilters } from './Constants';
@@ -11,12 +11,12 @@ export const userStatusesMap = writable(new Map<pubkey, Event[]>());
 export const userStatusReq = createRxBackwardReq();
 rxNostr
 	.use(userStatusReq.pipe(bufferTime(1000, null, maxFilters), batch()))
-	.pipe(uniq())
+	.pipe(
+		uniq(),
+		filter(({ event }) => !isExpired(event))
+	)
 	.subscribe((packet) => {
 		console.debug('[user status]', packet, packet.event.pubkey, packet.event.content);
-		if (isExpired(packet.event)) {
-			return;
-		}
 
 		const $userStatusesMap = get(userStatusesMap);
 		const statuses = $userStatusesMap.get(packet.event.pubkey);
