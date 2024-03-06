c341a944b1997b155bcc93aa178cee6b2a94b931
SnowCait <SnowCait@users.noreply.github.com>
2024-03-07 05:19:42 +0900

    Replay speed

--- web/src/routes/(app)/replay/home/+page.ts
+++ web/src/routes/(app)/replay/home/+page.ts
@@ -2,10 +2,13 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad<{
 	since: Date | null;
+	speed: number;
 }> = async ({ url }) => {
 	const since = url.searchParams.get('since');
+	const speed = Number(url.searchParams.get('speed'));
 	console.log('[replay page load]', since);
 	return {
-		since: since === null ? null : new Date(since)
+		since: since === null ? null : new Date(since),
+		speed: 0 < speed && speed <= 10 ? speed : 1
 	};
 };
