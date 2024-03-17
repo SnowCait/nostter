081e5ceffe74629b527987793c238e568f87183b
SnowCait <SnowCait@users.noreply.github.com>
2024-03-18 03:36:14 +0900

    Embedded YouTube

---
+++ web/src/lib/components/content/YouTube.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { ObserverRender } from '@svelteuidev/core';
+	import { enablePreview } from '../../../stores/Preference';
+	import ExternalLink from '../ExternalLink.svelte';
+
+	export let link: URL;
+
+	let id: string | undefined;
+
+	$: {
+		if (link.hostname === 'youtu.be') {
+			id = link.pathname.replace('/', '');
+		} else {
+			const v = link.searchParams.get('v');
+			if (v !== null) {
+				id = v;
+			}
+		}
+	}
+</script>
+
+<ObserverRender let:visible options={{ unobserveOnEnter: true }}>
+	{#if id !== undefined && visible && $enablePreview}
+		<iframe
+			id="ytplayer"
+			src="https://www.youtube.com/embed/{id}?origin={location.origin}"
+			frameborder="0"
+			title=""
+		/>
+	{:else}
+		<ExternalLink {link} />
+	{/if}
+</ObserverRender>
+
+<style>
+	iframe {
+		width: 100%;
+		aspect-ratio: 640 / 360;
+	}
+</style>
