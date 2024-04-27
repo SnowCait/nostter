4e05541ffc595c74384996dc167cd1b5abf4da1b
SnowCait <SnowCait@users.noreply.github.com>
2024-04-27 10:11:41 +0900

    Lists page

---
+++ web/src/routes/(app)/[slug=npub]/lists/+page.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import { createRxBackwardReq, latestEach } from 'rx-nostr';
+	import { Subscription, filter } from 'rxjs';
+	import type { Event } from 'nostr-typedef';
+	import { nip19 } from 'nostr-tools';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { metadataStore } from '$lib/cache/Events';
+	import { metadataReqEmit, rxNostr } from '$lib/timelines/MainTimeline';
+	import { reverseChronological } from '$lib/Constants';
+	import { filterTags, findIdentifier } from '$lib/EventHelper';
+	import type { LayoutData } from '../$types';
+	import Loading from '$lib/components/Loading.svelte';
+
+	export let data: LayoutData;
+
+	let listEvents = new Map<string, Event>();
+	let loading = true;
+	let subscription: Subscription | undefined;
+
+	$: metadata = $metadataStore.get(data.pubkey);
+	$: title =
+		(metadata === undefined ? '' : `${metadata.displayName}${$_('lists.particle')}`) +
+		$_('lists.title');
+
+	afterNavigate(() => {
+		console.log('[lists page]', data.pubkey);
+		listEvents.clear();
+		listEvents = listEvents;
+		loading = true;
+		metadataReqEmit([data.pubkey]);
+
+		const req = createRxBackwardReq();
+		subscription = rxNostr
+			.use(req)
+			.pipe(
+				latestEach(({ event }) => findIdentifier(event.tags) ?? ''),
+				filter(({ event }) => {
+					// For legacy clients
+					if (findIdentifier(event.tags) === 'mute') {
+						return false;
+					}
+					return event.tags.some(
+						([tagName, pubkey]) => tagName === 'p' && pubkey !== undefined
+					);
+				})
+			)
+			.subscribe({
+				next: ({ event }) => {
+					console.debug('[lists event]', event);
+					const identifier = findIdentifier(event.tags) ?? '';
+					const last = listEvents.get(identifier);
+					if (last === undefined || last.created_at < event.created_at) {
+						listEvents.set(identifier, event);
+						listEvents = listEvents;
+					}
+				},
+				complete: () => {
+					console.debug('[lists complete]');
+					loading = false;
+				}
+			});
+		req.emit([{ kinds: [30000], authors: [data.pubkey] }]);
+		req.over();
+	});
+
+	beforeNavigate(() => {
+		subscription?.unsubscribe();
+	});
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<ul>
+	{#each [...listEvents].map(([, event]) => event).toSorted(reverseChronological) as event}
+		<li class="card">
+			<a
+				href="/{nip19.npubEncode(data.pubkey)}/lists/{nip19.naddrEncode({
+					kind: event.kind,
+					pubkey: event.pubkey,
+					identifier: findIdentifier(event.tags) ?? ''
+				})}"
+			>
+				{filterTags('title', event.tags).at(0)?.at(1) ?? findIdentifier(event.tags) ?? '-'}
+			</a>
+		</li>
+	{:else}
+		{#if loading}
+			<Loading />
+		{:else}
+			<p class="card">{$_('lists.none')}</p>
+		{/if}
+	{/each}
+</ul>
