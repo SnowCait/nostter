da78cd01266ef4c62123dfc72169a3b736a12268
SnowCait <SnowCait@users.noreply.github.com>
2024-05-01 08:36:53 +0900

    Connection strategy

--- web/src/lib/timelines/MainTimeline.ts
+++ web/src/lib/timelines/MainTimeline.ts
@@ -24,6 +24,7 @@ import { Content } from '$lib/Content';
 import { sleep } from '$lib/Helper';
 
 export const rxNostr = createRxNostr({
+	connectionStrategy: 'lazy-keep',
 	eoseTimeout: timeout,
 	okTimeout: timeout,
 	retry: { strategy: 'immediately', maxCount: 1 },
