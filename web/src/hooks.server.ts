7bfa1d397abd0565425bcb59ec1191a1ab3f3c98
SnowCait <SnowCait@users.noreply.github.com>
2024-05-16 02:08:45 +0900

    Language

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { prepareStylesSSR } from '@svelteuidev/core';
 import { locale } from 'svelte-i18n';
+import { get } from 'svelte/store';
 
 const i18n: Handle = async ({ event, resolve }) => {
 	const lang = event.request.headers.get('accept-language')?.split(',')[0];
@@ -11,4 +12,10 @@ const i18n: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-export const handle: Handle = sequence(prepareStylesSSR, i18n);
+const lang: Handle = ({ event, resolve }) => {
+	return resolve(event, {
+		transformPageChunk: ({ html }) => html.replace('%lang%', get(locale) ?? 'en')
+	});
+};
+
+export const handle: Handle = sequence(prepareStylesSSR, i18n, lang);
