c97716e47a1f44d31b4f13754859b8994f14e786
SnowCait <SnowCait@users.noreply.github.com>
2024-07-04 08:49:33 +0900

    URL layout

--- web/src/lib/components/ExternalLink.svelte
+++ web/src/lib/components/ExternalLink.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-	import IconExternalLink from '@tabler/icons-svelte/dist/svelte/icons/IconExternalLink.svelte';
+	import '@tabler/icons-webfont/dist/tabler-icons.min.css';
 
 	export let link: URL;
+
+	$: content = link.hostname + link.pathname + link.search + link.hash;
+
+	const threshold = 64;
 </script>
 
 <a href={link.href} target="_blank" rel="noopener noreferrer">
-	<slot>{link.href}</slot><IconExternalLink size={15} />
+	<slot>
+		{content.length < threshold ? content : content.substring(0, threshold) + '...'}
+	</slot>
 </a>
+
+<style>
+	a::after {
+		font-family: 'tabler-icons';
+		content: '\ea99';
+	}
+</style>
