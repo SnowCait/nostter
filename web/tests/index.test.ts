0715f77e181bd16a2e18feb275107cd0569e9103
SnowCait <SnowCait@users.noreply.github.com>
2024-07-22 09:25:44 +0900

    Fix E2E test

--- web/tests/index.test.ts
+++ web/tests/index.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected logo', async ({ page }) => {
+test('index page has expected title', async ({ page }) => {
 	await page.goto('/');
 	await expect(page).toHaveTitle('nostter');
 });
