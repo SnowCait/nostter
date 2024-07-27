e9025be256cf82a185845ce3ca65b484a1a0ed76
SnowCait <SnowCait@users.noreply.github.com>
2024-07-27 11:54:07 +0900

    Revert "Merge pull request #1315 from SnowCait/sentry"
    This reverts commit 4c8ee9aa23a3553eb9c2401dc45cc750ebf48afc, reversing
    changes made to 2ad232565e556ba1a9145dd2231e723af9e4fc92.
    
    # Conflicts:
    #	web/.gitignore
    #	web/vite.config.ts

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,14 +1,8 @@
-import * as Sentry from '@sentry/sveltekit';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { prepareStylesSSR } from '@svelteuidev/core';
 import { locale } from 'svelte-i18n';
 import { get } from 'svelte/store';
-
-Sentry.init({
-	dsn: 'https://e2eed08417cadaede983f83fa498b6b8@o4507565086932992.ingest.us.sentry.io/4507565089292288',
-	tracesSampleRate: 1
-});
 
 const i18n: Handle = async ({ event, resolve }) => {
 	const lang = event.request.headers.get('accept-language')?.split(',')[0];
@@ -24,5 +18,4 @@ const lang: Handle = ({ event, resolve }) => {
 	});
 };
 
-export const handle: Handle = sequence(Sentry.sentryHandle(), prepareStylesSSR, i18n, lang);
-export const handleError = Sentry.handleErrorWithSentry();
+export const handle: Handle = sequence(prepareStylesSSR, i18n, lang);
