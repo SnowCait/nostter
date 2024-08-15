4e6e763536a8711cbc351912a640a61703694061
SnowCait <SnowCait@users.noreply.github.com>
2024-08-16 06:01:04 +0900

    Reduce REQs

--- web/src/lib/author/Action.ts
+++ web/src/lib/author/Action.ts
@@ -1,5 +1,5 @@
 import { get, writable } from 'svelte/store';
-import { batch, createRxBackwardReq, filterByKind, uniq } from 'rx-nostr';
+import { batch, createRxBackwardReq, filterByKind, uniq, type LazyFilter } from 'rx-nostr';
 import { bufferTime, bufferWhen, filter, interval } from 'rxjs';
 import type { Event } from 'nostr-typedef';
 import { rxNostr } from '$lib/timelines/MainTimeline';
@@ -7,7 +7,7 @@ import { maxFilters } from '$lib/Constants';
 import { filterTags, findReactionToId } from '$lib/EventHelper';
 import type { id } from '$lib/Types';
 import { pubkey } from '../stores/Author';
-import { storeDeletedEvents } from './Delete';
+import { deletedEventIdsByPubkey, storeDeletedEvents } from './Delete';
 
 export const repostedEventIds = writable(new Set<id>());
 export const reactionedEventIds = writable(new Set<id>());
@@ -64,17 +64,22 @@ export function authorActionReqEmit(event: Event): void {
 	const ids = [event.id, ...filterTags('e', event.tags)];
 	console.debug('[rx-nostr author action req]', ids);
 	const $pubkey = get(pubkey);
-	authorActionReq.emit([
+	const filters: LazyFilter[] = [
 		{
 			kinds: [6, 7],
 			authors: [$pubkey],
 			'#e': ids
-		},
+		}
+	];
+	const $deletedEventIdsByPubkey = get(deletedEventIdsByPubkey);
+	const $deletedEventIds = $deletedEventIdsByPubkey.get(event.pubkey);
+	if ($deletedEventIds === undefined || !$deletedEventIds.has(event.id)) {
 		// TODO: Move file because this is not author action
-		{
+		filters.push({
 			kinds: [5],
 			authors: [event.pubkey],
 			'#e': [event.id]
-		}
-	]);
+		});
+	}
+	authorActionReq.emit(filters);
 }
