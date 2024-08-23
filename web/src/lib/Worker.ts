970cba5d2f54b174b5d5c1fede3ab00254ddf54c
SnowCait <SnowCait@users.noreply.github.com>
2024-08-24 03:08:09 +0900

    Verify faster with WASM

--- web/src/lib/Worker.ts
+++ web/src/lib/Worker.ts
@@ -1,3 +1,12 @@
-import { startVerificationServiceHost } from 'rx-nostr-crypto';
+import { startVerificationServiceHost, type EventVerifier } from 'rx-nostr-crypto';
+import { Event as EventWrapper, loadWasmSync } from '@rust-nostr/nostr';
+import type { Event } from 'nostr-typedef';
 
-startVerificationServiceHost();
+loadWasmSync();
+
+const verifier: EventVerifier = async (event: Event) => {
+	const json = JSON.stringify(event);
+	return EventWrapper.fromJson(json).verify();
+};
+
+startVerificationServiceHost(verifier);
