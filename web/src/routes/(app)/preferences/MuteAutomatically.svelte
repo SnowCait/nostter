31640d9d6dac5d2c2716f157f8bccab0c71fa695
SnowCait <SnowCait@users.noreply.github.com>
2024-09-08 13:31:51 +0900

    Save mute automatically preference

---
+++ web/src/routes/(app)/preferences/MuteAutomatically.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import { preferencesStore, savePreferences } from '$lib/Preferences';
+
+	let enable = $preferencesStore.muteAutomatically;
+
+	async function changed(): Promise<void> {
+		console.log('[preferences mute automatically changed]', enable);
+		$preferencesStore.muteAutomatically = enable;
+		await savePreferences();
+	}
+</script>
+
+<label>
+	<input type="checkbox" bind:checked={enable} on:change={changed} />
+	<span>{$_('preferences.mute.automatically')}</span>
+</label>
