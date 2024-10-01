0540d93f25c14bc52fd852defd39a8121411c309
ocknamo <s14pes@gmail.com>
2024-10-01 23:59:28 +0900

    fix: remove FormatStyleFromObject

--- web/src/lib/components/content/Img.svelte
+++ web/src/lib/components/content/Img.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
 	import { imageOptimazerUrl } from '$lib/Constants';
 	import { imageOptimization } from '$lib/stores/Preference';
-	import { formatStyleFromObject } from '$lib/styles/FormatStyleFromObject';
 	import { Img, type ImgSrc } from 'svelte-remote-image';
 	export let src: string;
 	export let pathname: string;
-
-	const imgStyleObj = {
-		'max-width': 'calc(100% - 1.5em)',
-		'max-height': '20em',
-		margin: ' 0.5em',
-		border: '1px solid lightgray',
-		'border-radius': '5px',
-		'vertical-align': 'middle'
-	};
 
 	let imageSrc: ImgSrc = {
 		img: `${imageOptimazerUrl}width=800,quality=60,format=webp/${src}`,
@@ -23,8 +13,21 @@
 	let optimize = $imageOptimization;
 </script>
 
-{#if optimize && /\.(avif|jpg|jpeg|png|webp)$/i.test(pathname) && !src.startsWith(imageOptimazerUrl)}
-	<Img src={imageSrc} alt={src} style={formatStyleFromObject(imgStyleObj)} />
-{:else}
-	<img style={formatStyleFromObject(imgStyleObj)} {src} alt={src} />
-{/if}
+<div class="img-wrapper">
+	{#if optimize && /\.(avif|jpg|jpeg|png|webp)$/i.test(pathname) && !src.startsWith(imageOptimazerUrl)}
+	<Img src={imageSrc} alt={src} />
+	{:else}
+	<img {src} alt={src} />
+	{/if}
+</div>
+
+<style>
+	.img-wrapper :global(img){
+			max-width: calc(100% - 1.5em);
+			max-height: 20em;
+			margin:  0.5em;
+			border: 1px solid lightgray;
+			border-radius: 5px;
+			vertical-align: middle;
+	}
+</style>
--- web/src/lib/styles/FormatStyleFromObject.ts
+++
@@ -1,3 +0,0 @@
-export const formatStyleFromObject = (styleObj: Record<string, string>) => {
-	return Object.entries(styleObj).reduce((acc, [key, value]) => `${acc} ${key}: ${value};`, '');
-};
