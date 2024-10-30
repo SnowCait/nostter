115d0ebbf6aac7c1f9047f9481a83eb6d8a0dbf1
SnowCait <SnowCait@users.noreply.github.com>
2024-10-30 22:40:58 +0900

    Fix note link

--- web/src/lib/components/items/NoteLink.svelte
+++ web/src/lib/components/items/NoteLink.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
 	import { nip19 } from 'nostr-tools';
+	import { isNostrHex } from '$lib/EventHelper';
+	import { developerMode } from '$lib/stores/Preference';
 
 	export let eventId: string;
 </script>
 
 <article class="timeline-item">
-	<a href="/{nip19.neventEncode({ id: eventId })}">
-		{nip19.neventEncode({ id: eventId }).substring(0, 'nevent1'.length + 7)}
-	</a>
+	{#if isNostrHex(eventId)}
+		<a href="/{nip19.neventEncode({ id: eventId })}">
+			{nip19.neventEncode({ id: eventId }).substring(0, 'nevent1'.length + 7)}
+		</a>
+	{:else}
+		<div>{$_('content.not_found')}</div>
+		{#if $developerMode}
+			<div>{eventId}</div>
+		{/if}
+	{/if}
 </article>
