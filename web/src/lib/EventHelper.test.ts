c29160edff53f78711c92cc8a32f9db6def55252
SnowCait <SnowCait@users.noreply.github.com>
2024-11-21 08:50:55 +0900

    Support legacy e tags

--- web/src/lib/EventHelper.test.ts
+++ web/src/lib/EventHelper.test.ts
@@ -1,5 +1,100 @@
 import { describe, expect, it } from 'vitest';
-import { getZapperPubkey } from './EventHelper';
+import { getZapperPubkey, referTags } from './EventHelper';
+
+describe('referTags', () => {
+	it('root', () => {
+		const { root, reply } = referTags({
+			kind: 1,
+			pubkey: '',
+			content: '',
+			tags: [['e', 'root-id', '', 'root']],
+			created_at: 0,
+			id: '',
+			sig: ''
+		});
+		expect(root?.at(1)).toBe('root-id');
+		expect(reply?.at(1)).toBe(undefined);
+	});
+	it('reply', () => {
+		const { root, reply } = referTags({
+			kind: 1,
+			pubkey: '',
+			content: '',
+			tags: [
+				['e', 'root-id', '', 'root'],
+				['e', 'reply-id', '', 'reply']
+			],
+			created_at: 0,
+			id: '',
+			sig: ''
+		});
+		expect(root?.at(1)).toBe('root-id');
+		expect(reply?.at(1)).toBe('reply-id');
+	});
+	it('deprecated', () => {
+		const { root, reply } = referTags({
+			kind: 1,
+			pubkey: '',
+			content: '',
+			tags: [
+				['e', 'root-id'],
+				['e', 'reply-id']
+			],
+			created_at: 0,
+			id: '',
+			sig: ''
+		});
+		expect(root?.at(1)).toBe('root-id');
+		expect(reply?.at(1)).toBe('reply-id');
+	});
+	it('mixed', () => {
+		const { root, reply } = referTags({
+			kind: 1,
+			pubkey: '',
+			content: '',
+			tags: [
+				['e', 'root-id', '', 'root'],
+				['e', 'reply-id']
+			],
+			created_at: 0,
+			id: '',
+			sig: ''
+		});
+		expect(root?.at(1)).toBe('root-id');
+		expect(reply?.at(1)).toBe('reply-id');
+	});
+	it('invalid reply', () => {
+		const { root, reply } = referTags({
+			kind: 1,
+			pubkey: '',
+			content: '',
+			tags: [['e', 'root-id', '', 'reply']],
+			created_at: 0,
+			id: '',
+			sig: ''
+		});
+		expect(root?.at(1)).toBe('root-id');
+		expect(reply?.at(1)).toBe(undefined);
+	});
+	it('invalid duplicate marker', () => {
+		const { root, reply } = referTags({
+			kind: 1,
+			pubkey: '',
+			content: '',
+			tags: [
+				['e', 'root-id-1st', '', 'root'],
+				['e', 'root-id-2nd', '', 'root'],
+				['e', 'reply-id-1st', '', 'reply'],
+				['e', 'reply-id-2nd', '', 'reply']
+			],
+			created_at: 0,
+			id: '',
+			sig: ''
+		});
+		expect(root?.at(1)).toBe('root-id-2nd');
+		expect(reply?.at(1)).toBe('reply-id-2nd');
+	});
+});
 
 describe('getZapperPubkey', () => {
 	it('P tag', () => {
