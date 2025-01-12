d7b8f78be008c11bd789df16e9383a470605351f
雪猫 <SnowCait@users.noreply.github.com>
2025-01-12 22:09:23 +0900

    Revert "Revert "Revert "Fold long content"""

--- web/src/lib/nostr-tools/kinds.ts
+++
@@ -1,7 +0,0 @@
-export function isReplaceableKind(kind: number): boolean {
-	return [0, 3].includes(kind) || (10000 <= kind && kind < 20000);
-}
-
-export function isParameterizedReplaceableKind(kind: number): boolean {
-	return 30000 <= kind && kind < 40000;
-}
