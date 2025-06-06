07f95cadab6a912bdc43abf0a9afd3da5a5fcda1
SnowCait <SnowCait@users.noreply.github.com>
2025-06-06 14:46:46 +0900

    Seen on relays

---
+++ web/src/lib/components/SeenOnRelays.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { seenOn } from '$lib/timelines/MainTimeline';
+
+	export let id: string;
+
+	$: relays = seenOn.get(id) ?? new Set<string>();
+</script>
+
+<h5>Seen on {relays.size} relays</h5>
+
+{#each [...relays] as relay}
+	<div>{relay}</div>
+{/each}
