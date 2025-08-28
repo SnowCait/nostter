444b46f9b409994b361aafd58d7331be082b4681
SnowCait <SnowCait@users.noreply.github.com>
2025-08-29 00:05:39 +0900

    Color code hashtag

--- web/src/lib/components/content/Hashtag.svelte
+++ web/src/lib/components/content/Hashtag.svelte
@@ -3,3 +3,17 @@
 </script>
 
 <a href="/search?q={encodeURIComponent(`${text}`)}">{text}</a>
+{#if /^#[0-9a-fA-F]{6}$/.test(text)}
+	<span style="background-color: {text};"></span>
+{/if}
+
+<style>
+	span {
+		border: var(--default-border);
+		border-radius: 50%;
+		width: 1rem;
+		height: 1rem;
+		display: inline-block;
+		vertical-align: text-bottom;
+	}
+</style>
