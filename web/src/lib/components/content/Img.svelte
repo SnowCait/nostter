9310b39526c1304f71685e92575df73a87bb1a4e
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-12-15 03:49:22 +0900

    feat: hover gif

--- web/src/lib/components/content/Img.svelte
+++ web/src/lib/components/content/Img.svelte
@@ -5,6 +5,7 @@
 	import { getContext } from 'svelte';
 	import { _ } from 'svelte-i18n';
 	import { Img, type ImgSrc } from 'svelte-remote-image';
+	import { onMount } from 'svelte';
 
 	export let url: URL;
 
@@ -16,13 +17,26 @@
 		img: `${$imageOptimization}width=800,quality=60,format=webp/${src}`,
 		fallback: [src]
 	};
+
+	let imgElement: HTMLElement;
+
+	onMount(async () => {
+		if (/\.gif$/i.test(pathname) && imgElement) {
+			const FreezeFrame = (await import('freezeframe')).default;
+			new FreezeFrame(imgElement, {
+				trigger: 'hover',
+				overlay: true,
+				responsive: true
+			});
+		}
+	});
 </script>
 
-<span class="img-wrapper">
+<span class="img-wrapper" class:blur>
 	{#if $imageOptimization && /\.(avif|jpg|jpeg|png|webp)$/i.test(pathname) && !src.startsWith($imageOptimization)}
 		<Img class="global-content-image{blur ? ' blur' : ''}" src={imageSrc} alt={src} />
 	{:else}
-		<img class="global-content-image" class:blur {src} alt={src} />
+		<img bind:this={imgElement} class="global-content-image" {src} alt={src} />
 	{/if}
 	{#if blur}
 		<button>{$_('content.show')}</button>
@@ -30,7 +44,9 @@
 </span>
 
 <style>
-	.img-wrapper :global(.global-content-image) {
+	.img-wrapper :global(.global-content-image),
+	.img-wrapper :global(.ff-canvas),
+	.img-wrapper img {
 		max-width: calc(100% - 1.5em);
 		max-height: 20em;
 		margin: 0.5em;
@@ -39,13 +55,18 @@
 		vertical-align: middle;
 	}
 
-	.img-wrapper :global(.global-content-image.blur),
-	img.blur {
+	.img-wrapper.blur :global(.global-content-image.blur),
+	.img-wrapper.blur :global(.ff-canvas),
+	.img-wrapper.blur img {
 		filter: blur(8px);
 	}
 
 	.img-wrapper {
 		position: relative;
+	}
+
+	.img-wrapper :global(.ff-container) {
+		display: inline-block;
 	}
 
 	button {
