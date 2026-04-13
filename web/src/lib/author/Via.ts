f252e6b8d55963b75b798f134a60c3f5ecb32255
SnowCait <SnowCait@users.noreply.github.com>
2026-04-13 19:43:13 +0900

    Fix via

--- web/src/lib/author/Via.ts
+++ web/src/lib/author/Via.ts
@@ -1,5 +1,18 @@
 import { appName } from '$lib/Constants';
+import { persistedStore } from '$lib/WebStorage';
 import { Handlerinformation } from 'nostr-tools/kinds';
+
+export const ViaOption = ['none', 'once', 'always'] as const;
+export type ViaOption = (typeof ViaOption)[number];
+
+export const via = persistedStore<ViaOption>('via:kind:1', 'none', {
+	beforeWrite: (value) => {
+		if (value === 'once') {
+			return 'none';
+		}
+		return value;
+	}
+});
 
 export function createViaTag(): string[] {
 	return [
