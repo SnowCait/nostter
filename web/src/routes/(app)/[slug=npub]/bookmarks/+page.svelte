81a96923440a8cb0f07b0d811da0bcc96cde2b43
SnowCait <SnowCait@users.noreply.github.com>
2026-04-19 14:19:46 +0900

    Support NIP-44 encryption in bookmark
    Adjust logs

--- web/src/routes/(app)/[slug=npub]/bookmarks/+page.svelte
+++ web/src/routes/(app)/[slug=npub]/bookmarks/+page.svelte
@@ -3,23 +3,20 @@
 	import { tap } from 'rxjs';
 	import { _ } from 'svelte-i18n';
 	import { pubkey as authorPubkey, rom } from '$lib/stores/Author';
-	import { page } from '$app/stores';
 	import TimelineView from '../../TimelineView.svelte';
 	import { bookmarkEvent } from '$lib/author/Bookmark';
 	import { authorActionReqEmit } from '$lib/author/Action';
 	import { appName, reverseChronologicalItem } from '$lib/Constants';
 	import { filterTags } from '$lib/EventHelper';
-	import { Signer } from '$lib/Signer';
 	import { EventItem } from '$lib/Items';
 	import { referencesReqEmit, rxNostr, tie } from '$lib/timelines/MainTimeline';
 	import type { LayoutProps } from '../$types';
+	import { decryptListContent } from '$lib/List';
 
 	let { data }: LayoutProps = $props();
 
 	let publicBookmarkEventItems: EventItem[] = $state([]);
 	let privateBookmarkEventItems: EventItem[] = $state([]);
-
-	console.log('[bookmark page]', $page.params.slug);
 
 	// Public bookmarks
 	if ($bookmarkEvent !== undefined) {
@@ -43,7 +40,7 @@
 					})
 				)
 				.subscribe((packet) => {
-					console.log('[rx-nostr public bookmark packet]', packet);
+					console.debug('[bookmark public]', packet);
 					publicBookmarkEventItems.push(new EventItem(packet.event));
 					publicBookmarkEventItems =
 						publicBookmarkEventItems.sort(reverseChronologicalItem);
@@ -59,40 +56,35 @@
 			$bookmarkEvent !== undefined &&
 			$bookmarkEvent.content !== ''
 		) {
-			Signer.decrypt($authorPubkey, $bookmarkEvent.content)
-				.then((json) => JSON.parse(json) as string[][])
-				.then((tags) => {
-					const ids = filterTags('e', tags);
-					if (ids.length > 0) {
-						const eventsReq = createRxOneshotReq({
-							filters: [
-								{
-									ids
-								}
-							]
+			decryptListContent($authorPubkey, $bookmarkEvent.content).then(([tags]) => {
+				const ids = filterTags('e', tags);
+				if (ids.length > 0) {
+					const eventsReq = createRxOneshotReq({
+						filters: [
+							{
+								ids
+							}
+						]
+					});
+					rxNostr
+						.use(eventsReq)
+						.pipe(
+							tie,
+							uniq(),
+							tap(({ event }) => {
+								referencesReqEmit(event);
+								authorActionReqEmit(event);
+							})
+						)
+						.subscribe((packet) => {
+							console.debug('[bookmark private]', packet);
+							privateBookmarkEventItems.push(new EventItem(packet.event));
+							privateBookmarkEventItems = privateBookmarkEventItems.toSorted(
+								(a, b) => b.event.created_at - a.event.created_at
+							);
 						});
-						rxNostr
-							.use(eventsReq)
-							.pipe(
-								tie,
-								uniq(),
-								tap(({ event }) => {
-									referencesReqEmit(event);
-									authorActionReqEmit(event);
-								})
-							)
-							.subscribe((packet) => {
-								console.log('[rx-nostr private bookmark packet]', packet);
-								privateBookmarkEventItems.push(new EventItem(packet.event));
-								privateBookmarkEventItems = privateBookmarkEventItems.toSorted(
-									(a, b) => b.event.created_at - a.event.created_at
-								);
-							});
-					}
-				})
-				.catch((reason) => {
-					console.warn('[nip04.decrypt]', reason, $bookmarkEvent);
-				});
+				}
+			});
 		}
 	});
 </script>
