fc94ebabf2602094b7927610972b01f679ea8bdf
akazdayo <me@odango.app>
2026-08-14 03:40:16 +0900

    Upgrade Vite 7 to 8

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { name } from './package.json';
+import { name } from './package.json' with { type: 'json' };
 import { visualizer } from 'rollup-plugin-visualizer';
+
+const esbuildCssMinifier = {
+	name: 'esbuild-css-minifier',
+	config: () => ({ build: { cssMinify: 'esbuild' as const } })
+};
 
 export default defineConfig({
 	define: {
@@ -10,7 +15,11 @@ export default defineConfig({
 	build: {
 		target: 'esnext'
 	},
-	plugins: [sveltekit(), visualizer({ emitFile: true, template: 'markdown' })],
+	plugins: [
+		sveltekit(),
+		esbuildCssMinifier,
+		visualizer({ emitFile: true, template: 'markdown' })
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
