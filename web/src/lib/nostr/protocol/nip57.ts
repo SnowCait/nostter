c6bd33f0a06d873b6c5e5b3cfbccd42521b71d25
雪猫 <SnowCait@users.noreply.github.com>
2026-09-12 03:54:00 +0000

    Remove EventHelper dependency from NIP-57

--- web/src/lib/nostr/protocol/nip57.ts
+++ web/src/lib/nostr/protocol/nip57.ts
@@ -1,18 +1,21 @@
 import type { Event } from 'nostr-tools';
 import { Zap } from 'nostr-tools/kinds';
-import { filterTags } from '../../EventHelper';
 
 export function getZapSenderPubkey(event: Event): string | undefined {
 	if (event.kind !== Zap) {
 		return undefined;
 	}
 
-	const pubkey = filterTags('P', event.tags).at(0);
+	const pubkey = event.tags.find(
+		([name, value]) => name === 'P' && value !== undefined && value !== ''
+	)?.[1];
 	if (pubkey !== undefined) {
 		return pubkey;
 	}
 
-	const description = filterTags('description', event.tags).at(0);
+	const description = event.tags.find(
+		([name, value]) => name === 'description' && value !== undefined && value !== ''
+	)?.[1];
 	if (description === undefined) {
 		return undefined;
 	}
